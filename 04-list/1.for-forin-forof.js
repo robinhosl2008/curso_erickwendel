4f6caebf258197cc52768459b8082c5896790e30
robson.lourenco <robson.lourenco@pebmed.com.br>
2021-07-01 10:26:02 -0300

    Atualizando

--- 04-list/1.for-forin-forof.js
+++ 04-list/1.for-forin-forof.js
@@ -1,29 +1,36 @@
-const service = require('./service');
+const { ModuloBusca } = require('./service');
 
 async function main() {
     try {
+        const service = new ModuloBusca();
         const resultado = await service.obterPessoas('a');
 
         const names = [];
 
-        console.time('for');
-        for(let i = 0; i < resultado.results.length; i++){
-            const pessoa = resultado.results[i];
-            names.push(pessoa.name);
-        }
-        console.timeEnd('for');
+        // console.time('for');
+        // for(let i = 0; i < resultado.results.length; i++){
+        //     if(resultado.results.hasOwnProperty(i)){
+        //         const pessoa = resultado.results[i];
+        //         names.push(pessoa.name);
+        //     }
+        // }
+        // console.timeEnd('for');
 
-        console.time('for-in');
-        for(let i in resultado.results){
-            names.push(resultado.results[i].name);
-        }
-        console.timeEnd('for-in');
+        // console.time('for-in');
+        // for(let i in resultado.results){
+        //     if(resultado.results.hasOwnProperty(i)){
+        //         names.push(resultado.results[i].name);
+        //     }
+        // }
+        // console.timeEnd('for-in');
 
-        console.time('for-of');
+        // console.time('for-of');
         for(let pessoa of resultado.results){
-            names.push(pessoa.name);
+            if(pessoa.hasOwnProperty('name')){
+                names.push(pessoa.name);
+            }
         }
-        console.timeEnd('for-of');
+        // console.timeEnd('for-of');
         
         console.log('names:', names);
     } catch (error) {
--- 04-list/3.filter.js
+++ 04-list/3.filter.js
@@ -1,8 +1,8 @@
-// const { ModuloBusca } = require('./service');
+const { ModuloBusca } = require('./service');
 
 Array.prototype.myFilter = function(callback) {
     const lista = [];
-    for(let key in this){ console.log(typeof this[key])
+    for(let key in this){
         const item = this[key];
         const resultado = callback(item, key, this);
         if(!resultado) continue;
@@ -15,8 +15,8 @@ const myColorsArray = ['vermelho', 'azul', 'preto', 'verde', 'violeta'];
 
 async function main(nameOfColor){
     try {
-        // const service = new ModuloBusca();
-        // const resultado = await service.obterPessoas('a');
+        const service = new ModuloBusca();
+        const resultado = await service.obterPessoas('a');
 
         // const familiaLars = resultado.results.filter((item) => {
         //     // por padrão precisa retornar um booleano.
@@ -33,22 +33,22 @@ async function main(nameOfColor){
         //     return pessoa.name 
         // });
 
-        // const familiaLars = resultado.results.myFilter((pessoa, key) => {
-        //     if(pessoa.name.toLowerCase().indexOf('lars') !== -1){
-        //         return `key: ${key}, nome: ${pessoa.name}`;
-        //     }
-        // });
-
-        const listColors = myColorsArray.myFilter( item => {
-            if(typeof item !== "function" && item.indexOf(nameOfColor) !== -1){
-                return item;
+        const familiaLars = resultado.results.myFilter((pessoa, key) => {
+            if(pessoa.name.toLowerCase().indexOf('lars') !== -1){
+                return `key: ${key}, nome: ${pessoa.name}`;
             }
         });
 
-        console.log(listColors);
+        // const listColors = myColorsArray.myFilter( item => {
+        //     if(typeof item !== "function" && item.indexOf(nameOfColor) !== -1){
+        //         return item;
+        //     }
+        // });
+
+        console.log(familiaLars);
     } catch (error) {
         console.error("Deu ruim: ", error);
     }
 }
 
-main('p');
+main('v');
