8c3ee41c6c93d0e82aeccf4cb36cf69c2c64756b
robson.lourenco <robson.lourenco@pebmed.com.br>
2021-07-02 09:27:29 -0300

    Atualizando

--- 05-testes/test/index.test.js
+++ 05-testes/test/index.test.js
@@ -3,40 +3,25 @@ const mock = require('./index.mock');
 const { StarWars } = require('../utils/api_star_wars');
 const nock = require('nock');
 
+const apiStarWars = new StarWars();
 
-describe('Teste para saber se retornou os personagens.', () => {
+describe('Teste Star Wars.', () => {
     
-    beforeEach(() => {
+    let resposta = "";
+    beforeEach(async () => {
         nock('https://swapi.co/api/people')
             .get('/?search=r2-d2&format=json')
             .reply(200, mock());
+
+        resposta = await apiStarWars.buscaPersonagens('r2-d2');
     });
 
-    it('Deve buscar o R2-D2 com o formato correto.', async () => {
-        const apiStarWars = new StarWars();
-        const resposta = await apiStarWars.buscaPersonagens('r2-d2');
-        // .then((response) => {
-
-
-            // expect(typeof response).to.equal('object');
-
-
-            const resultado = resposta.results.map( (pessoa) => {
-                if(pessoa.hasOwnProperty('name', 'height')) {
-                    return { name: pessoa.name, peso: pessoa.height };
-                }
-            })
-
-        // })
-
-        
-        expect(resposta.results[0].hasOwnProperty('name')).to.equal(true, "Existe!");
-
-        // expect([{ name: 'R2-D2', peso: '96' }]).to.equal(resultado);
-        
-        // assert.deepStrictEqual(resultado, resultado, 'O nome não é igual.');
-
-        // assert.deepStrictEqual(resultado, [{ name: 'R2-D2', peso: '96' }], 'O R2-D2 não encontrado.');
+    it('Deve buscar o parâmetro "name" corretamente.', async () => {
+        assert.deepStrictEqual(resposta.results[0].hasOwnProperty('name'), true, 'Não retornou o parâmetro "name" corretamente!');
     });
 
+    it('Deve retornar no parâmetro "name" o nome "R2-D2".', async () => {
+        console.log(resposta.results[0].name)
+        assert.deepStrictEqual(resposta.results[0].name, 'R2-D2', 'O nome não é o esperado "R2-D2".')
+    });
 });
